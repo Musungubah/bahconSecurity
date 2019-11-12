6cd5c3a794c89f630c44412ddd0b5e4cdff4d0e2
user <user@this.com>
2019-11-12 14:08:12 -0500

    foxed fixed token

---
+++ src/main/java/com/bah/security/demo/api/TokenRequestData.java
@@ -0,0 +1,42 @@
+package com.bah.security.demo.api;
+
+public class TokenRequestData {
+	String username;
+	String password;
+	String scopes;
+	
+	public TokenRequestData() {
+		// TODO Auto-generated constructor stub
+	}
+	public TokenRequestData(String username, String password, String scopes) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.scopes = scopes;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getScopes() {
+		return scopes;
+	}
+	
+	public void setScopes(String scopes) {
+		this.scopes = scopes;
+	}	
+	
+}
